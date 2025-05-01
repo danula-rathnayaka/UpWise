0a4ce25c0f86a24120f15984e3484ecf91234c31
Danula <danusathmin@gmail.com>
2025-05-01 18:52:51 +0530

    ignore warnings (temp)

--- app/api/skillo/generate/route.ts
+++ app/api/skillo/generate/route.ts
@@ -9,8 +9,7 @@ export async function GET() {
 }
 
 export async function POST(request: Request) {
-  const { type, role, level, techstack, noOfQuestions, userid } =
-    await request.json();
+  const { type, role, level, techstack, amount, userid } = await request.json();
 
   try {
     const { text: questions } = await generateText({
@@ -20,7 +19,7 @@ export async function POST(request: Request) {
         The job experience level is ${level}.
         The tech stack used in the job is: ${techstack}.
         The focus between behavioural and technical questions should lean towards: ${type}.
-        The amount of questions required is: ${noOfQuestions}.
+        The amount of questions required is: ${amount}.
         Please return only the questions, without any additional text.
         The questions are going to be read by a voice assistant so do not use "/" or "*" or any other special characters which might break the voice assistant.
         Return the questions formatted like this:
